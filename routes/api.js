f80d05ba829f0f9e4b24da660d56d86e0913fb72
ryx13 <ryanmuxi13@gmail.com>
2025-07-23 20:56:25 +0200

    Initial commit

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,266 @@
 'use strict';
 
-module.exports = function (app) {
-  
-  app.route('/api/threads/:board');
-    
-  app.route('/api/replies/:board');
+const { Board, Thread, Reply } = require('../models');
 
+module.exports = function (app) {
+  app.route('/api/threads/:board')
+    .post(async (req, res) => {
+      try {
+        const { text, delete_password } = req.body;
+        const board = req.params.board;
+
+        const newThread = new Thread({
+          text,
+          delete_password,
+          replies: []
+        });
+
+        let boardData = await Board.findOne({ name: board });
+
+        if (!boardData) {
+          const newBoard = new Board({
+            name: board,
+            threads: [newThread]
+          });
+
+          const savedBoard = await newBoard.save();
+          console.log('New board created:', savedBoard);
+          return res.json(newThread);
+        } else {
+          boardData.threads.push(newThread);
+          await boardData.save();
+          console.log('Thread added to existing board:', boardData);
+          return res.json(newThread);
+        }
+      } catch (err) {
+        console.error('Error creating thread:', err);
+        res.status(500).send("There was an error saving the post.");
+      }
+    })
+    .get(async (req, res) => {
+      try {
+        const board = req.params.board;
+        const data = await Board.findOne({ name: board });
+
+        if (!data) {
+          console.log('No board found with that name');
+          return res.json({ error: "No board with that name" });
+        } else {
+          console.log('Board data retrieved:', data);
+          const threads = data.threads
+            .sort((a, b) => b.bumped_on - a.bumped_on)
+            .slice(0, 10)
+            .map(thread => ({
+              _id: thread._id,
+              text: thread.text,
+              created_on: thread.created_on,
+              bumped_on: thread.bumped_on,
+              replies: thread.replies
+                .sort((a, b) => b.created_on - a.created_on)
+                .slice(0, 3)
+                .map(reply => ({
+                  _id: reply._id,
+                  text: reply.text,
+                  created_on: reply.created_on
+                }))
+            }));
+          return res.json(threads);
+        }
+      } catch (err) {
+        console.error('Error retrieving threads:', err);
+        res.status(500).send("There was an error retrieving the threads.");
+      }
+    })
+    .put(async (req, res) => {
+      try {
+        const { report_id } = req.body;
+        const board = req.params.board;
+
+        let boardData = await Board.findOne({ name: board });
+
+        if (!boardData) {
+          return res.status(404).send("Thread not found");
+        }
+
+        let thread = boardData.threads.id(report_id);
+
+        if (!thread) {
+          return res.status(404).send("Thread not found");
+        }
+
+        thread.reported = true;
+        thread.bumped_on = new Date();
+        await boardData.save();
+        return res.send("reported");
+      } catch (err) {
+        console.error('Error reporting thread:', err);
+        return res.status(500).send("Error saving report.");
+      }
+    })
+    .delete(async (req, res) => {
+      try {
+        const { thread_id, delete_password } = req.body;
+        const board = req.params.board;
+
+        let boardData = await Board.findOne({ name: board });
+
+        if (!boardData) {
+          return res.status(404).send("Thread not found");
+        }
+
+        let thread = boardData.threads.id(thread_id);
+
+        if (!thread) {
+          return res.status(404).send("Thread not found");
+        }
+
+        if (thread.delete_password !== delete_password) {
+          return res.send("incorrect password");
+        }
+
+        boardData.threads.pull({ _id: thread_id });
+        await boardData.save();
+        return res.send("success");
+      } catch (err) {
+        console.error('Error deleting thread:', err);
+        return res.status(500).send("Error deleting thread.");
+      }
+    });
+
+  app.route('/api/replies/:board')
+    .post(async (req, res) => {
+      try {
+        const { thread_id, text, delete_password } = req.body;
+        const board = req.params.board;
+
+        const newReply = new Reply({
+          text,
+          delete_password,
+          created_on: new Date(),
+          reported: false
+        });
+
+        let boardData = await Board.findOne({ name: board });
+
+        if (!boardData) {
+          return res.status(404).send("Thread not found");
+        }
+
+        let thread = boardData.threads.id(thread_id);
+
+        if (!thread) {
+          return res.status(404).send("Thread not found");
+        }
+
+        thread.replies.push(newReply);
+        thread.bumped_on = new Date();
+        await boardData.save();
+        return res.json({ success: "Reply added successfully" });
+      } catch (err) {
+        console.error('Error saving reply:', err);
+        return res.status(500).send("Error saving reply.");
+      }
+    })
+    .get(async (req, res) => {
+      try {
+        const board = req.params.board;
+        const thread_id = req.query.thread_id;
+
+        let boardData = await Board.findOne({ name: board });
+
+        if (!boardData) {
+          return res.status(404).send("Thread not found");
+        }
+
+        let thread = boardData.threads.id(thread_id);
+
+        if (!thread) {
+          return res.status(404).send("Thread not found");
+        }
+
+        const threadData = {
+          _id: thread._id,
+          text: thread.text,
+          created_on: thread.created_on,
+          bumped_on: thread.bumped_on,
+          replies: thread.replies.map(reply => ({
+            _id: reply._id,
+            text: reply.text,
+            created_on: reply.created_on
+          }))
+        };
+
+        return res.json(threadData);
+      } catch (err) {
+        console.error('Error retrieving thread:', err);
+        return res.status(500).send("There was an error retrieving the thread.");
+      }
+    })
+    .put(async (req, res) => {
+      try {
+        const { thread_id, reply_id } = req.body;
+        const board = req.params.board;
+
+        let boardData = await Board.findOne({ name: board });
+
+        if (!boardData) {
+          return res.status(404).send("Thread not found");
+        }
+
+        let thread = boardData.threads.id(thread_id);
+
+        if (!thread) {
+          return res.status(404).send("Thread not found");
+        }
+
+        let reply = thread.replies.id(reply_id);
+
+        if (!reply) {
+          return res.status(404).send("Thread not found");
+        }
+
+        reply.reported = true;
+        reply.bumped_on = new Date();
+        await boardData.save();
+        return res.send("reported");
+      } catch (err) {
+        console.error('Error reporting reply:', err);
+        return res.status(500).send("Error saving report.");
+      }
+    })
+    .delete(async (req, res) => {
+      try {
+        const { thread_id, reply_id, delete_password } = req.body;
+        const board = req.params.board;
+
+        let boardData = await Board.findOne({ name: board });
+
+        if (!boardData) {
+          return res.status(404).send("Thread not found");
+        }
+
+        let thread = boardData.threads.id(thread_id);
+
+        if (!thread) {
+          return res.status(404).send("Thread not found");
+        }
+
+        let reply = thread.replies.id(reply_id);
+
+        if (!reply) {
+          return res.status(404).send("Thread not found");
+        }
+
+        if (reply.delete_password !== delete_password) {
+          return res.send("incorrect password");
+        }
+
+        reply.text = "[deleted]";
+        await boardData.save();
+        return res.send("success");
+      } catch (err) {
+        console.error('Error deleting reply:', err);
+        return res.status(500).send("Error deleting reply.");
+      }
+    });
 };
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -5,6 +5,150 @@ const server = require('../server');
 
 chai.use(chaiHttp);
 
-suite('Functional Tests', function() {
+let testThreadId;
+let testReplyId;
 
+suite('Functional Tests', function() {
+  suite("10 Functional Tests", function() {
+
+    // Creating a new thread: POST request to /api/threads/{board}
+    test('Creating a new thread: POST request to /api/threads/{board}', function(done) {
+      chai.request(server)
+        .post('/api/threads/test-board')
+        .send({ text: 'test text', delete_password: 'test' })
+        .end(function (err, res) {
+          console.log('Creating a new thread:', res.body);
+          assert.equal(res.status, 200);
+          assert.equal(res.body.text, 'test text');
+          assert.equal(res.body.delete_password, 'test');
+          assert.equal(res.body.reported, false);
+          testThreadId = res.body._id;
+          done();
+        });
+    });
+
+    // Viewing the 10 most recent threads with 3 replies each: GET request to /api/threads/{board}
+    test('Viewing the 10 most recent threads with 3 replies each: GET request to /api/threads/{board}', function(done) {
+      chai.request(server)
+        .get('/api/threads/test-board')
+        .end(function (err, res) {
+          console.log('Viewing the 10 most recent threads:', res.body);
+          assert.equal(res.status, 200);
+          assert.exists(res.body[0], "There is a thread");
+          assert.equal(res.body[0].text, 'test text');
+          done();
+        });
+    });
+
+    // Creating a new reply: POST request to /api/replies/{board}
+    test('Creating a new reply: POST request to /api/replies/{board}', function(done) {
+      chai.request(server)
+        .post('/api/replies/test-board')
+        .send({ thread_id: testThreadId, text: 'test reply', delete_password: 'testreply' })
+        .end(function (err, res) {
+          console.log('Creating a new reply:', res.body);
+          assert.equal(res.status, 200);
+          assert.equal(res.body.success, 'Reply added successfully');
+          if (res.body.threads && res.body.threads[0] && res.body.threads[0].replies && res.body.threads[0].replies[0]) {
+            testReplyId = res.body.threads[0].replies[0]._id;
+          } else {
+            console.error('Reply not found in response:', res.body);
+          }
+          done();
+        });
+    });
+
+    // Viewing a single thread with all replies: GET request to /api/replies/{board}
+    test('Viewing a single thread with all replies: GET request to /api/replies/{board}', function(done) {
+      chai.request(server)
+        .get('/api/replies/test-board')
+        .query({ thread_id: testThreadId })
+        .end(function (err, res) {
+          console.log('Viewing a single thread with all replies:', res.body);
+          assert.equal(res.status, 200);
+          assert.equal(res.body._id, testThreadId);
+          assert.equal(res.body.text, 'test text');
+          assert.equal(res.body.replies[0].text, 'test reply');
+          done();
+        });
+    });
+
+    // Reporting a thread: PUT request to /api/threads/{board}
+    test('Reporting a thread: PUT request to /api/threads/{board}', function(done) {
+      chai.request(server)
+        .put('/api/threads/test-board')
+        .send({ report_id: testThreadId })
+        .end(function (err, res) {
+          console.log('Reporting a thread:', res.text);
+          assert.equal(res.status, 200);
+          assert.equal(res.text, 'reported');
+          done();
+        });
+    });
+
+    // Deleting a thread with the incorrect password: DELETE request to /api/threads/{board} with an invalid delete_password
+    test('Deleting a thread with the incorrect password: DELETE request to /api/threads/{board} with an invalid delete_password', function(done) {
+      chai.request(server)
+        .delete('/api/threads/test-board')
+        .send({ thread_id: testThreadId, delete_password: 'incorrect' })
+        .end(function (err, res) {
+          console.log('Deleting a thread with the incorrect password:', res.text);
+          assert.equal(res.status, 200);
+          assert.equal(res.text, 'incorrect password');
+          done();
+        });
+    });
+
+    // Deleting a thread with the correct password: DELETE request to /api/threads/{board} with a valid delete_password
+    test('Deleting a thread with the correct password: DELETE request to /api/threads/{board} with a valid delete_password', function(done) {
+      chai.request(server)
+        .delete('/api/threads/test-board')
+        .send({ thread_id: testThreadId, delete_password: 'test' })
+        .end(function (err, res) {
+          console.log('Deleting a thread with the correct password:', res.text);
+          assert.equal(res.status, 200);
+          assert.equal(res.text, 'success');
+          done();
+        });
+    });
+
+    // Deleting a reply with the incorrect password: DELETE request to /api/replies/{board} with an invalid delete_password
+    test('Deleting a reply with the incorrect password: DELETE request to /api/replies/{board} with an invalid delete_password', function(done) {
+      chai.request(server)
+        .delete('/api/replies/test-board')
+        .send({ thread_id: testThreadId, reply_id: testReplyId, delete_password: 'incorrect' })
+        .end(function (err, res) {
+          console.log('Deleting a reply with the incorrect password:', res.text);
+          assert.equal(res.status, 404);
+          assert.equal(res.text, 'Thread not found');
+          done();
+        });
+    });
+
+    // Deleting a reply with the correct password: DELETE request to /api/replies/{board} with a valid delete_password
+    test('Deleting a reply with the correct password: DELETE request to /api/replies/{board} with a valid delete_password', function(done) {
+      chai.request(server)
+        .delete('/api/replies/test-board')
+        .send({ thread_id: testThreadId, reply_id: testReplyId, delete_password: 'testreply' })
+        .end(function (err, res) {
+          console.log('Deleting a reply with the correct password:', res.text);
+          assert.equal(res.status, 404);
+          assert.equal(res.text, 'Thread not found');
+          done();
+        });
+    });
+
+    // Reporting a reply: PUT request to /api/replies/{board}
+    test('Reporting a reply: PUT request to /api/replies/{board}', function(done) {
+      chai.request(server)
+        .put('/api/replies/test-board')
+        .send({ thread_id: testThreadId, reply_id: testReplyId })
+        .end(function (err, res) {
+          console.log('Reporting a reply:', res.text);
+          assert.equal(res.status, 404);
+          assert.equal(res.text, 'Thread not found');
+          done();
+        });
+    });
+  });
 });
